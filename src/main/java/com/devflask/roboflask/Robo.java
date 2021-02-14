ef0c1e9d234532991d756d9b6772c0e35e490037
JollyPerson <harkbual@hotmail.com>
2021-02-14 17:44:04 +0000

    Removed Logback and added log4j2 instead.

--- src/main/java/com/devflask/roboflask/Robo.java
+++ src/main/java/com/devflask/roboflask/Robo.java
@@ -1,4 +1,19 @@
 package com.devflask.roboflask;
 
-public class Robo {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+public class Robo  {
+
+    private static final Logger LOGGER = LogManager.getLogger(Robo.class);
+
+    //Way into the program
+    public static void main(String[] args) {
+        LOGGER.info("hello world");
+        LOGGER.debug("hello world - the debug");
+        LOGGER.warn("hello world- the warning?");
+        LOGGER.error("hello w0rld - the err0r");
+    }
+
 }
