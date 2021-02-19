e20a5ef568f7bd87ed543a5176035b5b52849a1b
JollyPerson <harkbual@hotmail.com>
2021-02-19 16:47:36 +0000

    Added Env token

--- src/main/java/com/devflask/roboflask/Robo.java
+++ src/main/java/com/devflask/roboflask/Robo.java
@@ -10,11 +10,16 @@ public class Robo  {
 
     private static final Logger LOGGER = LogManager.getLogger(Robo.class);
     private final Bot bot;
+
     //Way into the program
     public static void main(String[] args) {
         try {
             // TODO: BotInstance bot = new Robo();
-            Robo robo = new Robo(args[0]); //Token is set by args for now
+            if(args.length == 0){
+                Robo robo = new Robo(System.getenv("TOKEN"));
+            }else {
+                Robo robo = new Robo(args[0]); //Token is set by args for now
+            }
 
         } catch (LoginException | InterruptedException e) {
             e.printStackTrace();
