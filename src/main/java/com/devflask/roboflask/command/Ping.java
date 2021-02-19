e9ac457a68b63d72c0bed97ccd6edd4b39478232
dehys <arijan.nikoci@gmail.com>
2021-02-19 04:02:23 +0100

    imported logger and added 2 execute methods with according events

--- src/main/java/com/devflask/roboflask/command/Ping.java
+++ src/main/java/com/devflask/roboflask/command/Ping.java
@@ -2,11 +2,16 @@ package com.devflask.roboflask.command;
 
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.priv.PrivateMessageReceivedEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.HashSet;
 import java.util.Set;
 
 public class Ping implements Command{
+
+    private static final Logger LOGGER = LogManager.getLogger(Ping.class);
 
     @Override
     public String getName() {
@@ -27,7 +32,14 @@ public class Ping implements Command{
 
     @Override
     public void execute(GuildMessageReceivedEvent event) {
-        event.getChannel().sendMessage("pong").queue();
+        event.getChannel().sendMessage("pong!").queue();
+        LOGGER.debug("PONG in guild channel with id: "+event.getChannel().getId());
+    }
+
+    @Override
+    public void execute(PrivateMessageReceivedEvent event) {
+        event.getChannel().sendMessage("pong!").queue();
+        LOGGER.debug("PONG in privatechannel with id: "+event.getChannel().getId());
     }
 
     @Override
