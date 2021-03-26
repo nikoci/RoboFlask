6d8564c48e0bf7a7d92b57bca0675fa732796b7a
JollyPerson <jollyperson_real@Outlook.om>
2021-03-26 00:53:29 +0000

    Kick command now works.

--- src/main/java/com/devflask/roboflask/command/moderation/Kick.java
+++ src/main/java/com/devflask/roboflask/command/moderation/Kick.java
@@ -7,10 +7,14 @@ import com.devflask.roboflask.util.EmbedColor;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.MessageBuilder;
 import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.priv.PrivateMessageReceivedEvent;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -31,8 +35,49 @@ public class Kick implements Command {
     }
 
     public void execute(GuildMessageReceivedEvent event) {
-        //
-    }
+            final TextChannel channel = event.getChannel();
+            final Message message = event.getMessage();
+            final Member member = event.getMember();
+            final Member target = message.getMentionedMembers().isEmpty() ? null : message.getMentionedMembers().get(0);
+            final String[] args = message.getContentRaw().split(" ");
+
+
+            if (target == null || message.getMentionedMembers().isEmpty()){
+                channel.sendMessage(MessageUtil.getCommandError(
+                        MessageUtil.Messages.COMMAND_ERROR_USAGE,
+                        member.getEffectiveName(),
+                        member.getUser().getAvatarUrl(),
+                        "!kick <user> [reason]"
+                        ).build()
+                ).queue();
+                return;
+            }
+
+            if (!member.canInteract(target) || !event.getGuild().getSelfMember().canInteract(target)){
+                channel.sendMessage(MessageUtil.getPermissionError(
+                        MessageUtil.Messages.PERMISSION_ERROR_HIERARCHY,
+                        this.getRequiredPermissions(),
+                        member.getEffectiveName(),
+                        member.getUser().getAvatarUrl()
+                        ).clearFields()
+                                .build()
+                ).queue();
+                return;
+            }
+
+            final String reason = String.join(" ", Arrays.asList(args).subList(2, args.length));
+            target.kick(reason).queue();
+            channel.sendMessage(MessageUtil.getCommandSuccess(
+                    MessageUtil.Messages.COMMAND_SUCCESS_KICK,
+                    member.getEffectiveName(),
+                    member.getUser().getAvatarUrl(),
+                    target.getEffectiveName(),
+                    " for ",
+                    reason
+                    ).build()
+            ).queue();
+        }
+
 
     @Override
     public void execute(CommandInformation info){
@@ -41,6 +86,8 @@ public class Kick implements Command {
 
     @Override
     public @NotNull Set<Permission> getRequiredPermissions() {
-        return new HashSet<>();
+        Set<Permission> permissions = new HashSet<>();
+        permissions.add(Permission.KICK_MEMBERS);
+        return permissions;
     }
 }
