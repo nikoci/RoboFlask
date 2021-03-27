e921dbfff56253caa135caffef92989ee53bd1bc
JollyPerson <jollyperson_real@Outlook.com>
2021-03-27 17:46:31 +0000

    MySQL and DevflaskAPI datasources
    Signed-off-by: JollyPerson <jollyperson_real@Outlook.com>

---
+++ src/main/kotlin/com/devflask/roboflask/database/DevflaskAPIDatabase.kt
@@ -0,0 +1,35 @@
+package com.devflask.roboflask.database
+
+class DevflaskAPIDatabase : DatabaseManager {
+    override fun getPrefix(id: Long): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun setPrefix(id: Long, prefix: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getCommands(id: Long): Collection<CustomCommand> {
+        TODO("Not yet implemented")
+    }
+
+    override fun addCommand(id: Long, command: CustomCommand) {
+        TODO("Not yet implemented")
+    }
+
+    override fun isLogEnabled(id: Long): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun setLogEnabled(id: Long, value: Boolean) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getLogChannelID(id: Long): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun setLogChannelID(id: Long, channelID: Long) {
+        TODO("Not yet implemented")
+    }
+}
